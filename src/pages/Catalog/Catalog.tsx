06bba4a6175367fc258ac97254985b19a254a654
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-12-28 01:40:25 +0300

    added topbar

---
+++ src/pages/Catalog/Catalog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TopBar } from '../../components/TopBar'
+import { Categories } from '../../components/Categories'
+
+export const Catalog = () => {
+    return (
+        <div>
+            <TopBar/>
+            <Categories />
+        </div>
+    )
+}
