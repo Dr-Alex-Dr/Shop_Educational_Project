b695c11e4e6e787cc9658534a42a9feb899b76f0
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-12-31 05:06:12 +0500

    fixed url

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, HashRouter } from 'react-router-dom';
 import { Catalog } from './pages/Catalog/Catalog';
 import ProductCard from './pages/ProductCard/ProductCard';
 import TopBar from './components/TopBar/TopBar';
 import { IGoodInfo } from './interfaces';
 
-const App = () => {
+const App = () =>{
   const [favourites, setFavourites] = useState<IGoodInfo[]>([])
   const [cart, setCart] = useState<IGoodInfo[]>([])
 
   return (
     <div style={{maxWidth: 1170}}>
-      <Router>
-        <TopBar favourites={favourites} cart={cart} setCart={setCart}/>
-        <Switch>
-          <Route path="/" exact render={(props)=><Catalog {...props} favourites={favourites} setFavourites={setFavourites} cart={cart} setCart={setCart}/>}/>
-          <Route path="/product/:id" render={(props)=><ProductCard {...props} favourites={favourites} setFavourites={setFavourites} cart={cart} setCart={setCart}/>}/>
-        </Switch>
-      </Router>
+      <HashRouter>
+        <Router>
+          <TopBar favourites={favourites} cart={cart} setCart={setCart}/>
+          <Switch>
+            <Route path="/" exact render={(props)=><Catalog {...props} favourites={favourites} setFavourites={setFavourites} cart={cart} setCart={setCart}/>}/>
+            <Route path="/product/:id" render={(props)=><ProductCard {...props} favourites={favourites} setFavourites={setFavourites} cart={cart} setCart={setCart}/>}/>
+          </Switch>
+        </Router>
+      </HashRouter>
     </div>
   );
 }
